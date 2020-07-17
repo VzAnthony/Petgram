b7e9f2c960ea44ea36292358ca97457e7000505d
Anthony Moreno <anthony560@hotmail.com>
2020-07-17 19:17:24 -0400

    inicio de proyecyo e implemento de configuraciones

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  output: {
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html')
+    })
+  ]
+}
