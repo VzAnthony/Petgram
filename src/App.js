8147a97ea48a6cb2edb62a9fafff3b1fc56b8a49
Anthony Moreno <anthony560@hotmail.com>
2020-07-22 14:56:10 -0400

    agregando las PhotoCard ademas de poner react-icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyle'
+import { PhotoCard } from './components/PhotoCard'
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <ListOfCategories />
+      <PhotoCard />
     </>
   )
 }
