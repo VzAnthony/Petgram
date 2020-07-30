b035e45ccf311e6788f611a8f6492dd64dbc91ae
Anthony Moreno <anthony560@hotmail.com>
2020-07-30 16:57:12 -0400

    peticion con @apollo/client

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
+import { gql, useQuery } from '@apollo/client'
+
+const getPhotos = gql`
+query getPhotos {
+  photos {
+    id
+    categoryId
+    src
+    likes
+    userId
+    liked
+  }
+}
+`
 
 export const ListOfPhotoCards = () => {
+  const { loading, error, data : { photos = [] } = {}} = useQuery(getPhotos)
+  console.log(photos)
   return (
-    <ul>
-      {[1, 2, 3, 4, 5, 6, 7].map(id => <PhotoCard key={id} id={id} />)}
-    </ul>
+    loading 
+      ? <ul>
+        {[1].map(id => <PhotoCard key={id} id={id} />)}
+      </ul>
+      : <ul>
+        {photos.map(photo => <PhotoCard key={photo.id} {...photo} />)}
+      </ul>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://petgram-server-anthony-3vrjckvsb.vercel.app/graphql',
+  cache: new InMemoryCache()
+})
 
 render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('app')
 )
