c37f2f618d6fd7d566f87201a392671f5e6d2793
Anthony Moreno <anthony560@hotmail.com>
2020-08-04 17:52:20 -0400

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react'
 import { GlobalStyle } from './GlobalStyle'
 import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+import { NavBar } from './components/NavBar'
 import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './components/PhotoCardWithQuery'
 import { Router } from '@reach/router'
 
 const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
   return (
     <>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Router>
-            <Home path='/' />
-            <Home path='/pet/:id' />
-          </Router>
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+      </Router>
+      <NavBar />
     </>
   )
 }
