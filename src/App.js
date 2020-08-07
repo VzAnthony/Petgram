82ac9be91bb01bb506eea94e2eeeca9db8f8eea6
Anthony Moreno <anthony560@hotmail.com>
2020-08-06 23:39:42 -0400

    rutas para usuarios registrados y no registrados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Router } from '@reach/router'
 import { GlobalStyle } from './GlobalStyle'
-import { Home } from './pages/Home'
-import { Detail } from './pages/Detail'
 import { NavBar } from './components/NavBar'
 import { Logo } from './components/Logo'
-import { Router } from '@reach/router'
+
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 const App = () => {
   return (
@@ -16,6 +24,21 @@ const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              : <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+        }
+      </UserLogged>
       <NavBar />
     </>
   )
