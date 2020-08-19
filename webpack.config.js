1d6499fe7ee537a206e0614b7b20285b8ab063c3
Anthony Moreno <anthony560@hotmail.com>
2020-08-19 16:07:51 -0400

    soporte Offline

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   output: {
@@ -25,11 +26,29 @@ module.exports = {
       short_name: 'Petgram 🐶',
       description: 'Con Petgram puedes encontrar fotos de animales domesticos',
       background_color: '#fff',
-      theme_color: '#b1a',
+      theme_color: '#0f4c75',
       icons: [
         {
           src: path.resolve('src/assets/icon.png'),
           sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://https://petgram-server-anthony-3vrjckvsb.vercel.app/'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
         }
       ]
     })
